db06f765d1934ff848da013d1e3a7baa5fda6fe4
Miroslav Lichvar <mlichvar@redhat.com>
2009-03-17 13:12:23 +0100

    update copyright years

--- misc.c
+++ misc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008  Miroslav Lichvar <mlichvar@redhat.com>
+ * Copyright (C) 2008, 2009  Miroslav Lichvar <mlichvar@redhat.com>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
